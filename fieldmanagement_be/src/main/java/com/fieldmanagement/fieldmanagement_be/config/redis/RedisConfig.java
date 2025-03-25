c4975de51cda6f96f81e5e5360033360b721469e
giangtx03 <giangtx.2003@gmail.com>
2025-03-25 15:06:10 +0700

    Config redis

---
+++ fieldmanagement_be/src/main/java/com/fieldmanagement/fieldmanagement_be/config/redis/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.fieldmanagement.fieldmanagement_be.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(){
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+
+        jedisConnectionFactory.getPoolConfig().setMaxTotal(10);
+        jedisConnectionFactory.getPoolConfig().setMaxIdle(5);
+        jedisConnectionFactory.getPoolConfig().setMinIdle(1);
+
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
